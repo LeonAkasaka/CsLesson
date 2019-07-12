a64b45beb358f8a4117f424488e3f962d73b6a92
Leon Akasaka <leon@wisdomsoft.jp>
2019-07-12 17:24:48 +0900

    テストひな型を追加。

--- src/TimeCalc/TimeCalcUnitTest/ActivityPointUnitTest.cs
+++ src/TimeCalc/TimeCalcUnitTest/ActivityPointUnitTest.cs
@@ -1,13 +1,26 @@
+using System;
+using System.Collections.Generic;
+using TimeCalc;
 using Xunit;
 
 namespace TimeCalcUnitTest
 {
     public class ActivityPointUnitTest
     {
-        [Fact]
-        public void GetTotalRecoveryIntervalTest()
+        public static IEnumerable<object[]> GetTotalRecoveryIntervalTestData
         {
+            get
+            {
+                yield return new object[] { new ActivityPoint(0, 0, TimeSpan.Zero), TimeSpan.Zero };
+            }
+        }
 
+        [Theory]
+        [MemberData(nameof(GetTotalRecoveryIntervalTestData))]
+        public void GetTotalRecoveryIntervalTest(ActivityPoint pt, TimeSpan expected)
+        {
+            var actual = pt.GetTotalRecoveryInterval();
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
