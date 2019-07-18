51197c12f4f432fb1adf648bc88e6ffae2a69af1
Leon Akasaka <leon@wisdomsoft.jp>
2019-07-19 03:00:10 +0900

    TimeCalc のテスト追加。

--- src/TimeCalc/TimeCalcUnitTest/ActivityPointUnitTest.cs
+++ src/TimeCalc/TimeCalcUnitTest/ActivityPointUnitTest.cs
@@ -12,6 +12,15 @@ namespace TimeCalcUnitTest
             get
             {
                 yield return new object[] { new ActivityPoint(0, 0, TimeSpan.Zero), TimeSpan.Zero };
+                yield return new object[] { new ActivityPoint(0, 1, TimeSpan.FromMinutes(1)), TimeSpan.FromMinutes(1) };
+                yield return new object[] { new ActivityPoint(0, 10, TimeSpan.FromMinutes(1)), TimeSpan.FromMinutes(10) };
+                yield return new object[] { new ActivityPoint(0, 10, TimeSpan.Zero), TimeSpan.Zero };
+                yield return new object[] { new ActivityPoint(30, 50, TimeSpan.FromMinutes(1)), TimeSpan.FromMinutes(20) };
+                yield return new object[] { new ActivityPoint(150, 100, TimeSpan.FromMinutes(1)), TimeSpan.Zero };
+                yield return new object[] { new ActivityPoint(10, 70, TimeSpan.FromSeconds(1)), TimeSpan.FromMinutes(1) };
+                yield return new object[] { new ActivityPoint(10, 10, TimeSpan.FromSeconds(1)), TimeSpan.Zero };
+                yield return new object[] { new ActivityPoint(0, 10, TimeSpan.FromSeconds(-1)), TimeSpan.FromSeconds(-10) };
+                yield return new object[] { new ActivityPoint(100, 250, TimeSpan.FromTicks(3)), TimeSpan.FromTicks(450) };
             }
         }
 
@@ -23,10 +32,25 @@ namespace TimeCalcUnitTest
             Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void GetMaxRevoveryTimeTest()
+        public static IEnumerable<object[]> GetMaxRevoveryTimeTestData
         {
+            get
+            {
+                var now = DateTimeOffset.Now;
+                yield return new object[] { new ActivityPoint(0, 0, TimeSpan.Zero), now, now };
+                yield return new object[] { new ActivityPoint(0, 1, TimeSpan.FromMinutes(1)), now, now.AddMinutes(1) };
+                yield return new object[] { new ActivityPoint(0, 10, TimeSpan.FromMinutes(1)), now, now.AddMinutes(10) };
+                yield return new object[] { new ActivityPoint(30, 54, TimeSpan.FromHours(1)), now, now.AddDays(1) };
+                yield return new object[] { new ActivityPoint(100, 100, TimeSpan.FromHours(1)), now, now };
+            }
+        }
 
+        [Theory]
+        [MemberData(nameof(GetMaxRevoveryTimeTestData))]
+        public void GetMaxRevoveryTimeTest(ActivityPoint pt, DateTimeOffset now, DateTimeOffset expected)
+        {
+            var actual = pt.GetMaxRevoveryTime(now);
+            Assert.Equal(expected, actual);
         }
     }
 }
